b90b5464f2ee1e681550dcc62d356fdeaa98f3c9
Yves <byiringiroyves127@gmail.com>
2019-12-28 20:59:58 +0200

    add

--- report/admin.py
+++ report/admin.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from .models import ReportType, Report
 
+class ReportAdminb(admin.ModelAdmin):
+        list_display = ('report_type','igihe_itangirwa','deadline',)
 
 class ReportAdmin(admin.ModelAdmin):
         list_display = ('report_type','report_file','submited_date',)
 
 admin.site.register(Report, ReportAdmin)
 
-admin.site.register(ReportType)
+admin.site.register(ReportType,ReportAdminb)
--- report/scheduler.py
+++ report/scheduler.py
@@ -3,9 +3,15 @@ from apscheduler.schedulers.background import BackgroundScheduler
 from .models import * 
 
 def start():
-    nun = getattr(ReportType, 'deadline')
-    
-    print(nun)
+    report = ReportType.objects.values('deadline')
+
+   
+    # nun = getattr(ReportType, 'deadline')
+
+    print('============================================')
+    print(report)
+    print('============================================')
+    print('hsssssssssss')
     scheduler = BackgroundScheduler()
-    scheduler.add_job(mail, 'date', run_date=date(2019,12,28) )
+    scheduler.add_job(mail, 'date', run_date=date(2019,12,27) )
     scheduler.start()
