6fbd61a89f69cb92e3d61873ef2f078d5da79955
Taufique <iradukunda.ta@gmail.com>
2019-12-30 21:51:04 +0200

    add test

--- report/scheduler.py
+++ report/scheduler.py
@@ -1,10 +1,11 @@
 from datetime import datetime
 from apscheduler.schedulers.background import BackgroundScheduler
-from .models import * 
+
+from .models import *
 
 def start():
     nun = getattr(ReportType, 'deadline')
-    
+
     print(nun)
     scheduler = BackgroundScheduler()
     scheduler.add_job(mail, 'date', run_date=date(2019,12,28) )
